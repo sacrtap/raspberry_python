0ce3a9e2a4bd79780c0c128304a483ff8f75ec69
sacrtap <sacrtap@hotmail.com>
2017-03-30 13:19:09 +0800

    add test file

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ total = 0;  # 这是一个全局变量
 def sum(arg1, arg2):
     # 返回2个参数的和."
     total = arg1 + arg2;  # total在这里是局部变量.
-    print "函数内是局部变量 : ", total
+    print "函数内是局部变量 : ", total, total
     return total
 
 
