7ba8241ca5a861b6e66f55e868cbea586eb66a6d
sacrtap <sacrtap@hotmail.com>
2017-03-30 15:58:10 +0800

    add MyEventClass

--- carServer.py
+++ carServer.py
@@ -5,3 +5,17 @@
 # @Site    : 
 # @File    : carServer.py
 # @Software: PyCharm
+
+from wsgiref.simple_server import make_server
+
+
+def simple_app(environ, start_response):
+  status = '200 OK'
+  response_headers = [('Content-type','text/plain')]
+  start_response(status, response_headers)
+  return environ['PATH_INFO'][1:]
+  #return ['Hello world!\n']
+
+
+httpd = make_server('127.0.0.1', 8082, simple_app)
+httpd.serve_forever()
