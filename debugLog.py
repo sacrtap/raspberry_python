320aee67982812f90a47f0831f4cbc8ab5c0df38
sacrtap <sacrtap@hotmail.com>
2017-04-05 15:12:46 +0800

    new

--- debugLog.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2017/4/5 15:01
-# @Author  : Aries
-# @Site    : 
-# @File    : debugLog.py
-# @Software: PyCharm
--- index.py
+++ index.py
@@ -8,23 +8,12 @@
 # git pull origin master
 
 import web
-import GPIOControl
+#import GPIOControl
 import logging
+import logging.config
 
-logging.basicConfig(level=logging.DEBUG,
-                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
-                datefmt='%a, %d %b %Y %H:%M:%S',
-                filename='debug.log',
-                filemode='w')
-
-#################################################################################################
-#定义一个StreamHandler，将INFO级别或更高的日志信息打印到标准错误，并将其添加到当前的日志处理对象#
-console = logging.StreamHandler()
-console.setLevel(logging.INFO)
-formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
-console.setFormatter(formatter)
-logging.getLogger('').addHandler(console)
-#################################################################################################
+logging.config.fileConfig("logger.conf")
+logger = logging.getLogger("example01")
 
 urls = ("/.*", "hello")  # 指定任何url都指向hello类
 app = web.application(urls, globals())  # 绑定url
@@ -33,6 +22,7 @@ app = web.application(urls, globals())  # 绑定url
 class hello:
     def GET(self):
         logging.info('web service is ok.')
+        logging.debug('web service is ok.')
         return "hello"
 
 if __name__ == "__main__":
