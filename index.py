fe5c7031caab15945f1aa4f05eba276233c051df
sacrtap <sacrtap@hotmail.com>
2017-04-13 14:38:44 +0800

    给定connect方法

--- index.py
+++ index.py
@@ -8,32 +8,42 @@
 # git pull origin master
 
 import web
-import GPIOControl
+#import GPIOControl
 import logging
 import logging.config
+import os
 
 logging.config.fileConfig("logger.conf")
 logger = logging.getLogger("example01")
 
-controller = GPIOControl.GPIOController()
+#controller = GPIOControl.GPIOController()
 
 urls = ("/", "hello",
-        "/runForward", "runForward"
+        "/connect", "connect"
         )  # 指定任何url都指向hello类
 
 app = web.application(urls, globals())
 application = app.wsgifunc()
+app_root = os.path.dirname(__file__)
+templates_root = os.path.join(app_root, 'templates')
+render = web.template.render(templates_root)
 
 # 定义相应类
 class hello:
     def GET(self):
-        logging.info('web service is connected.')
-        return "hello"
+        logging.info('web service root.')
+        return "web service root"
 
-class runForward:
+class connect:
     def GET(self):
-        controller.runForward(50)
-        return "running"
+        _status = False
+        _frequency = 0
+        data = web.input()
+        _frequency = data.get('frequency')
+        if _frequency:
+            _status = True
+        web.header('Content-Type', 'text/json')
+        return render.connect(_status, _frequency)
 
 if __name__ == "__main__":
     app.run()
